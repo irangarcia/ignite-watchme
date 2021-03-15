00a7c2e5ab08d8761116968f931ec426fec68e17
Iran Garcia <irangarciaj@gmail.com>
2021-03-15 18:06:45 -0300

    :sparkles: Initial commit
    Componetizing the application

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,38 @@
+import { Button } from './Button';
+import { GenreResponseProps } from '../App';
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  setSelectedGenreId(id: number): void;
+}
+
+export function SideBar(props: SideBarProps) {
+  const { genres, selectedGenreId, setSelectedGenreId } = props;
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre: { id: any; title: any; name: any }) => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
